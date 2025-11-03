5a7324fe07bd623b91a4f947661b5238fb3f6106
Carolina Xavier <carolinaxavier.xx@gmail.com>
2025-11-03 14:03:20 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -626,7 +626,7 @@ export default function App() {
                                         {disciplina}
                                       </div>
 
-                                      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
+                                      <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
                                         <div>
                                           <label className="text-xs text-gray-600 block mb-1">
                                             CH Teoria (h)
